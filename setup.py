438e52013b8bc4946e4c7231bb0bf9913cd0c3c6
EthanTheMathmo <eh595@cam.ac.uk>
2021-09-16 23:30:11 +0100

    started building tests and fixing bug
    -started writing unit tests
    -fixed bug in the conversion from base 26 without a 0 (i.e. not Excel style notation) to Excel style notation for counting which was breaking c. 0.3% of conversions

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ with open("README.md", "r") as readme_file:
 requirements = ["PySimpleGUI>=4.0.0", "xlwings>=0.24.0", "openpyxl>=3.0.0"]
 
 setup(
-    name="notebookc",
-    version="0.0.1",
-    author="Jeff Hale",
-    author_email="jeffmshale@gmail.com",
-    description="A package to convert your Jupyter Notebook",
+    name="Excelutilities",
+    version="0.0.2",
+    author="Ethan Horsfall",
+    author_email="ethan.horsfall@gmail.com",
+    description="Useful functions for writing code for Excel applications",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/EthanTheMathmo/excel-utilities/",
