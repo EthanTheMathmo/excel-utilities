124029e6e239efae435065a0154be687c1b0b916
EthanTheMathmo <eh595@cam.ac.uk>
2021-10-15 23:37:12 +0100

    user experience improvement by distributing imports

--- Excelutilities/importing_data_helpers.py
+++ Excelutilities/importing_data_helpers.py
@@ -2,9 +2,7 @@
 These are helper functions for when we have rows or columns in one spreadsheet we want 
 to bring across into another
 """
-import xlwings as xw
-import PySimpleGUI as sg
-import sys
+from tkinter.constants import TRUE
 from Excelutilities.index_helpers import is_col_block_bool,  is_row_block_bool
 import decimal
 import datetime
@@ -16,15 +14,17 @@ def add_col_data_from_another_sheet(col_name_1, col_name_2, allowed_data_types=[
 
     allowed_data_types is to restrict the types values can take.
     """
-
-    sg.popup_ok(f"Please select the input {col_name_1} and click 'ok' when finished")
+    import PySimpleGUI as sg
+    sg.popup_ok(f"Please select the input {col_name_1} and click 'ok' when finished", keep_on_top=True)
+    import xlwings as xw
     input_col_name_1 = xw.apps.active.books.active.selection.value
     input_col_name_1_address = xw.apps.active.books.active.selection.address
-    sg.popup_ok(f"Please select the input {col_name_2} and click 'ok' when finished")
+    sg.popup_ok(f"Please select the input {col_name_2} and click 'ok' when finished", keep_on_top=True)
     active_cells = xw.apps.active.books.active.selection
     input_col_name_2 = active_cells.value
     input_col_name_2_address = active_cells.address
 
+    import sys
 
     def sanity_check_col_entries(entry1, entry2, addresses_and_names):
         #entry1 is a list of values
@@ -32,17 +32,17 @@ def add_col_data_from_another_sheet(col_name_1, col_name_2, allowed_data_types=[
         #is the address, and second is the name
         #implements some sanity checks
         if len(entry1) != len(entry2):
-            sg.popup("Oops! Your two input columns of data are of different lengths.\nNow terminating...")
+            sg.popup("Oops! Your two input columns of data are of different lengths.\nNow terminating...", keep_on_top=True)
             sys.exit()
 
 
         for val1, val2 in zip(entry1, entry2):
             if type(val1) not in allowed_data_types and val1 != None:
-                sg.popup(f"Oops you selected some data which we couldn' recognise\nValue: {val1}")
+                sg.popup(f"Oops you selected some data which we couldn' recognise\nValue: {val1}", keep_on_top=True)
                 sys.exit()
 
             if type(val2) not in allowed_data_types and val2 != None:
-                sg.popup(f"Oops you selected some data which we couldn' recognise\n{val2}\n{type(val2)}")
+                sg.popup(f"Oops you selected some data which we couldn' recognise\n{val2}\n{type(val2)}", keep_on_top=True)
                 sys.exit()
         
         for address_and_name in addresses_and_names:
@@ -50,7 +50,7 @@ def add_col_data_from_another_sheet(col_name_1, col_name_2, allowed_data_types=[
             name = address_and_name[1]
             if not is_col_block_bool(address):
                 print(address)
-                sg.popup(f"Oops! Your {name} data wasn't from a single column")
+                sg.popup(f"Oops! Your {name} data wasn't from a single column", keep_on_top=True)
                 sys.exit()
 
     #sanity checks
@@ -69,7 +69,7 @@ def add_col_data_from_another_sheet(col_name_1, col_name_2, allowed_data_types=[
         col_name_1_to_col_name_2[val1] = val2
         
 
-    sg.popup_ok("Please select the output col_name_1s and click 'ok' when finished")
+    sg.popup_ok(f"Please select the {col_name_1} and click 'ok' when finished", keep_on_top=True)
     output_col_name_1 = xw.apps.active.books.active.selection.value
 
 
@@ -80,6 +80,8 @@ def add_col_data_from_another_sheet(col_name_1, col_name_2, allowed_data_types=[
             return None
     output_array = [[helper_col_name_1(val1)] for val1 in output_col_name_1]
 
-    sg.popup_ok(f"Please select the output location for the {col_name_2} and click ok when finished\nThis should be the same length as your input {col_name_1} data!")
+    sg.popup_ok(f"Please select the output location for the {col_name_2} and click ok when finished\nThis should be the same length as your input {col_name_1} data!", keep_on_top=True)
     xw.apps.active.books.active.selection.value = output_array
 
+if __name__ == "__main__":
+    add_col_data_from_another_sheet("A", "B")
